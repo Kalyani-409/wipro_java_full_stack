258298470f8b5a0e7b38e5c799698e7859b24cfb
Kalyani-409 <byrukalyani3@gmail.com>
2025-07-14 23:12:07 +0530

    Add files via upload

---
+++ TypeCasting.java
@@ -0,0 +1,9 @@
+import java.util.*;
+public class TypeCasting {
+    public static void main(String[] args){
+     Scanner sc=new Scanner(System.in);
+     char var=sc.next().charAt(0);
+     System.out.println((int)var);
+
+    }
+}
